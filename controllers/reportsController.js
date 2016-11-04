fcd464854a4d802c1e529ab4d17cd9d7ea457b8d
Venkatesh <venkateshjkuratti@gmail.com>
2016-11-04 16:38:08 -0500

    Traffic cop viewer layout updates

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -1,6 +1,6 @@
 ImgVisionApp.controller('reportsController', function($scope,$http,$timeout,$sce){
 
-$scope.iFrameReportUrl = "";
+$scope.iFrameReportUrl = $sce.trustAsResourceUrl("https://bi.dev.corp.pep.pvt/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=FhRjEFgxkAsAv0AAABCnsFAkAFBWhAAt");
 $scope.reportCategories = [
     {reportName: "Document Data Dump Report" , url: "https://bi.dev.corp.pep.pvt/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=FhRjEFgxkAsAv0AAABCnsFAkAFBWhAAt"  },
     {reportName: "Document Transition History Data Dump Report", url:"https://bi.dev.corp.pep.pvt/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=Fit6BFj10gIARg4AABC3wfUkAFBWhAAt" },
